4825dc36e1eaf461886c95ba667eb5227d2d9e57
friend <metalhead2112001@gmail.com>
2021-09-28 09:34:00 +0300

    Add project files.

---
+++ OPTeht7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace OPTeht7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+              
+                bool jatkuu;
+                do
+                {
+                    Console.Write("Anna säde: ");
+                    string vastaanotettu = Console.ReadLine();
+                    double kelpo;
+                    //Säteen tulla olla positiivinen luku
+                    while (!Double.TryParse(vastaanotettu, out kelpo) || kelpo<0)
+                    {
+                        Console.Write("Virheellinen arvo, yritä vielä: ");
+                        vastaanotettu = Console.ReadLine();
+                    }
+                //Nyt voit luoda olion:
+                Ympyra ympyra = new Ympyra(kelpo);
+                Console.WriteLine("Säde: {0:F3}, pinta-ala: {1:F3}.",
+                   ympyra.Sade, ympyra.LaskeAla());
+
+
+
+
+                Console.Write("Jatketaanko K/E?: ");
+                    string valinta = Console.ReadLine().ToUpper();
+                    if (valinta.StartsWith("K"))
+                        jatkuu = true;
+                    else
+                        jatkuu = false;
+                } while (jatkuu);
+            
+        }
+    }
+
+    internal class Ympyra
+    {
+        internal int Sade;
+        private double kelpo;
+
+        public Ympyra(double kelpo)
+        {
+            this.kelpo = kelpo;
+        }
+
+        internal int LaskeAla()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OPTeht7/Ympyra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPTeht8A
+{
+    class Ympyra
+    {
+        //Muodostin, constructor
+        public Ympyra(double sade)
+        {
+            this.sade = sade;
+        }
+
+        //Ominaisuudet, property
+        public double sade { get; set; }
+
+        //Luokan metodit, instance method
+        public double LaskeAla()
+        {
+            return Math.PI * sade * sade;
+
+        }
+        
+
+    }
+}
